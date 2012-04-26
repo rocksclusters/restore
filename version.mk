90a65ed6c7f5b4ae0a031289702d3621d9e5f943
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-04-26 20:30:19 +0000

    wildcard xorg.conf. Doesn't exist on CentOS 6

--- version.mk
+++ version.mk
@@ -14,6 +14,7 @@ CONTRIB_PKG_DIR	= $(shell /opt/rocks/bin/rocks report distro)/contrib
 # these are the files that will be restored when this roll is supplied
 # during installation
 #
-FILES	+= /etc/X11/xorg.conf $(wildcard /var/named/*local)
+FILES	+= $(wildcard /etc/X11/xorg.conf) 
+FILES	+= $(wildcard /var/named/*local)
 FILES	+= $(wildcard /etc/ssh/*key*)
 SCRIPTS	+=
